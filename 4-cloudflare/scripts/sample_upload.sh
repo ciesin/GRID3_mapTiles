c500ffe9a9103d9537e6c2ac1b8ab2692614802e
Matthew Heaton <mjh2241@columbia.edu>
2026-02-05 14:34:45 -0500

    init cloudflare remote

---
+++ 4-cloudflare/scripts/sample_upload.sh
@@ -0,0 +1,1 @@
+rclone copyto buildings.pmtiles grid3-tiles-rclone:grid3-tiles/buildings.pmtiles --progress --s3-no-check-bucket --s3-chunk-size=256M --header-upload "Content-Type: application/vnd.pmtiles"
